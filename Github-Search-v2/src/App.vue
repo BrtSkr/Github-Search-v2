9b59a2563d7ebbe5a3849e230b3d2253383c5a40
Bartosz Skrzydelski <brtskr1503@gmail.com>
2023-03-09 04:28:02 +0100

    Added initial API call

--- Github-Search-v2/src/App.vue
+++ Github-Search-v2/src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <GithubData @search="handleSearch" :api-data="apiData"/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style lang="scss" scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import { defineComponent } from 'vue'
+import GithubData from './components/GithubData.vue';
+import axios from 'axios'
+
+export default defineComponent({
+  components: { GithubData },
+  data() {
+  return {
+    apiData: null
+  }
+  },
+  methods: {
+    async handleSearch(value) {
+      try {
+        const response = await axios.get(`https://api.github.com/users/${value}`)
+        console.log(response.data);
+        this.apiData = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+})
+</script>
---
+++ Github-Search-v2/src/components/GithubData.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input type="text" v-model="inputValue">
+        <button @click="$emit('search', inputValue)">Search</button>
+        <h1>{{ apiData }}</h1>
+    </div>
+</template>
+<script setup>
+defineProps({
+    username: String
+})
+</script>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+    data() {
+        return {
+            inputValue: ''
+        }
+    },
+    props: {
+    apiData: {
+        type: Object,
+        default: null
+    }
+    },
+    methods: {
+        handleClick() {
+            this.$emit('search', this.inputValue)
+        }
+    }
+})
+</script>
